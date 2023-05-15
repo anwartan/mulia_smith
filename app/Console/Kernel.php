1957eacf3365b38a563c07bba19113f06e428b38
anwartanjung <anwar.tanjung@gdn-commerce.com>
2023-05-15 16:12:00 +0700

    add schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,9 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->call(function () {
-            Log::info('Minute log message');
-        })->everyMinute();
+        $schedule->command('fetch:gold-price')->dailyAt('14:00');
     }
 
     /**
